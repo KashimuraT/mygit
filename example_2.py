cdec41989e624c60420b1671d39acc8c725c2c9a
bp18030 <bp18030@shibaura-it.ac.jp>
2020-10-26 15:01:13 +0800

    change example_2.py

--- example_2.py
+++ example_2.py
@@ -1,4 +1,5 @@
 age = int(input('Enter your age in years: '))
 max_hear_rate =206.9 -(0.67 * age)
-target = 0.65 * max_hear_rate
+#target = 0.65 * max_hear_rate
+target = 0.5 * max_hear_rate
 print('Your target fat-buruning heart is :', target)
